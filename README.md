87145549f7018e8eb63bd5d1201ac42160fae5b5
Marc-Emmanuel PERRIN <marc-emmanuel.perrin@telecom-paristech.fr>
2016-04-14 14:28:47 +0200

    Install in README.md

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
 
 ### Installation
 
+Depending on how you want to use it for. At the moment, ``amel-compiler`` can be used either as a standalone compiler or as a npm module inside a project.
+
+#### Standalone
+
+For a standalone compilation utility, you just have to clone the repo, and use run.js : 
+```shell
+$ cd path/to/compiler
+$ node run.js file.amel 
+```
+
+This will ouput a ``file.html``
+
+You can alias it or export it to your PATH variable if you want to use it globaly as a system command.
+
+#### As npm module inside another project
+
+To use as a runtime dynamic compiler, you may use ``amel-compiler`` as a npm module inside you nodejs app. For now the module is not registered on npmjs.com, so you will have to copy it directly in the ``node_modules/`` directory of your project which imply to package it with your app if you want to distribute it (or to avoid echo-ing ``node_modules`` to your ``.gitignore``). 
+
+To install it as an npm module with the dedicated npm command line util, open a shell (UNIX):
+
+```shell 
+$ cd path/to/compiler
+$ npm install --prefix /path/to/your/project amel-compiler
+```
+
+This will install ``amel-compiler`` in a ``node_modules/``directory of your project (it maybe easier to prefix an absolute path). 
+
 ### Running
 
 ### Cheat sheet
--- test.js
+++ test.js
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 
 // Requirements
-var Fs         = require( "fs" );
-var Path       = require( "path" );
-var Crypto     = require( "crypto" );
-var Parser     = require( "./modules/parser.js" );
-var Logger     = require( "./modules/logger.js" );
-var testTotal  = 0;
+var Fs = require("fs");
+var Path = require("path");
+var Crypto = require("crypto");
+var Parser = require("./modules/parser.js");
+var Logger = require("./modules/logger.js");
+var testTotal = 0;
 var testPassed = 0;
-var logger     = new Logger();
+var logger = new Logger();
 var scriptName = __filename;
-scriptName     = scriptName.replace( __dirname + "/", "" );
+scriptName = scriptName.replace(__dirname + "/", "");
 
 // For each files in the test folder, parse it and run compare callback on EOF.
-Fs.readdir( "tests", function( err, items ) {
-    if ( err ) {
+Fs.readdir("tests", function (err, items) {
+    if (err) {
         throw err
     }
-    for ( var i = 0 ; i < items.length ; i++ ) {
+    for (var i = 0; i < items.length; i++) {
         // Only parse .amel files
-        if ( Path.extname( items[i]) === ".amel" ) {
+        if (Path.extname(items[i]) === ".amel") {
             parser = new Parser();
-            parser.parse( "./tests/" + items[i], compare );
+            parser.parse("./tests/" + items[i], compare);
         }
     }
-//     logger.log( "--------------------\n" + testPassed + " / " + testTotal + " tests passed" );
+    //     logger.log( "--------------------\n" + testPassed + " / " + testTotal + " tests passed" );
 });
 
 /**
@@ -36,44 +36,44 @@ Fs.readdir( "tests", function( err, items ) {
  * @param Input file
  * @return None
  */
-var compare = function( _item ) {
+var compare = function (_item) {
     var item = _item.path;
     testTotal++;
-    var testName    = item.replace( ".amel", "" );
-    testName        = item.replace( "./tests/", "" );
-    var resFile     = item.replace( ".amel", ".res" );
-    var htmlFile    = item.replace( ".amel", ".html" );
-    var resContent  = "";
+    var testName = item.replace(".amel", "");
+    testName = item.replace("./tests/", "");
+    var resFile = item.replace(".amel", ".res");
+    var htmlFile = item.replace(".amel", ".html");
+    var resContent = "";
     var htmlContent = "";
     // If res file does not exists
     try {
-        Fs.accessSync( resFile, Fs.F_OK );
-        resContent = Fs.readFileSync( resFile );
-    } catch ( e ) {
-        logger.log( testName + ": Res file ( " + resFile + 
-                    " ) not accessible", scriptName, "e" );
+        Fs.accessSync(resFile, Fs.F_OK);
+        resContent = Fs.readFileSync(resFile);
+    } catch (e) {
+        logger.log(testName + ": Res file ( " + resFile +
+            " ) not accessible", scriptName, "e");
     }
     // If html file was not generated
     try {
-        Fs.accessSync( htmlFile, Fs.F_OK );
-        htmlContent = Fs.readFileSync( htmlFile );
-    } catch ( e ) {
-        logger.log( testName + ": HTML file (" + htmlFile + 
-                    ") not accessible", scriptName, "e" );
+        Fs.accessSync(htmlFile, Fs.F_OK);
+        htmlContent = Fs.readFileSync(htmlFile);
+    } catch (e) {
+        logger.log(testName + ": HTML file (" + htmlFile +
+            ") not accessible", scriptName, "e");
     }
-    
+
     // Compute and compare MD5 sums of the generated HTML file and expected result
-    var challengedMD5 = Crypto.createHash( "md5" )
-                              .update( htmlContent )
-                              .digest( "hex" );
-    var resMD5        = Crypto.createHash( "md5" )
-                              .update( resContent )
-                              .digest( "hex" );
+    var challengedMD5 = Crypto.createHash("md5")
+        .update(htmlContent)
+        .digest("hex");
+    var resMD5 = Crypto.createHash("md5")
+        .update(resContent)
+        .digest("hex");
     // Print result
-    if ( challengedMD5 === resMD5 ) {
-        logger.log( "- " + testName + " [\x1b[32m√\x1b[0m]", scriptName );
+    if (challengedMD5 === resMD5) {
+        logger.log("- " + testName + " [\x1b[32m√\x1b[0m]", scriptName);
         testPassed++;
     } else {
-        logger.log( "- " + testName + " [\x1b[31mX\x1b[0m]", scriptName );
+        logger.log("- " + testName + " [\x1b[31mX\x1b[0m]", scriptName);
     }
 }
