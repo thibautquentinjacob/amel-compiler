0f0f9144458ca64ba7b1b1b3083209a131475814
Marc-Emmanuel PERRIN <marc-emmanuel.perrin@telecom-paristech.fr>
2016-04-14 16:13:41 +0200

    added posibility to use parser by passing amel text directly

--- c.js
+++
@@ -1,7 +0,0 @@
-var p = require('./modules/parser.js');
-var amel = new p();
-
-var text = require('fs').readFileSync('./example/site.amel', 'utf-8');
-amel.parse(text, function(obj){
-    console.log(obj.output);
-});
