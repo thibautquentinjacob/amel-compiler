e894a0f0cf631245a878ad77e435504596547108
Thibaut Jacob <thibaut.jacob@telecom-paristech.fr>
2016-04-21 11:20:30 +0200

    - Removed log.txt from tracking.

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var Parser = require( "./modules/parser.js" );
-var parser = new Parser();
+var parser = new Parser( true );
 if ( process.argv.length > 2 ) {
     parser.parse( process.argv[2], function() {
         
